7a1d8d753432327e7d21c9f73fa1406e4d1a3e2b
Andrei Radu <raduandrei697@gmail.com>
2024-07-29 23:49:25 +0300

    change update posts/users dtos

--- apps/posts/src/dto/update-post.input.ts
+++ apps/posts/src/dto/update-post.input.ts
@@ -3,6 +3,9 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdatePostInput extends PartialType(CreatePostInput) {
-  @Field(() => Int)
-  id: number;
+  @Field()
+  id: string;
+
+  @Field()
+  body?: string;
 }
